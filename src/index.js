9818e9e7c56c49f9fb607e98572c985e6a8395e3
Imad Jerra <smartshader@gmail.com>
2019-01-25 18:37:37 -0500

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import '@babel/polyfill';
+import http from 'http';
+
+function requestHandler(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestHandler);
+server.listen(8080);
