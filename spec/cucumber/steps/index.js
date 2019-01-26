c6d99ce0367d4d7ae4c4a193b891ac45b8ec3437
Imad Jerra <smartshader@gmail.com>
2019-01-26 12:44:45 -0500

    Check validity of email for Create User endpoint

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -63,6 +63,23 @@ When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are)(\
         .set('Content-Type', 'application/json');
 });
 
+When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are) exactly (.+)$/, function (payloadType, fields, value) {
+    const payload = {
+        email: 'e@ma.il',
+        password: 'password',
+    };
+
+    const fieldsToModify = fields.split(',').map(s => s.trim()).filter(s => s !== '');
+    fieldsToModify.forEach((field) => {
+        payload[field] = value;
+    });
+
+    this.request
+        .send(JSON.stringify(payload))
+        .set('Content-Type', 'application/json');
+
+});
+
 When(/^without a (?:"|')([\w-]+)(?:"|') header set$/, function (headerName) {
     this.request.unset(headerName);
 });
