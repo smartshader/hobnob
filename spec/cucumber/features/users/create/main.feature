d6c18f846b5b38cfb1d02537de5d0884c2fb43ff
Imad Jerra <smartshader@gmail.com>
2019-01-25 22:22:56 -0500

    Handle malformed/non-JSON payloads for POST /user

--- spec/cucumber/features/users/create/main.feature
+++ spec/cucumber/features/users/create/main.feature
@@ -2,11 +2,18 @@ Feature: Create User
 
   Clients should be able to send a request to our API in order to create a user. Our API should also validate the structure of the payload and respond with an error if it is invalid.
 
-  Scenario: Empty Payload
-    If the client sends a POST request to /users with a unsupported payload, it should receive a response with a 4xx status code.
+  Scenario Outline: Bad Client Requests
+    If the client sends a POST request to /users with an empty, non-JSON, or malformed payload, it should receive a response with a 4xx Bad Request HTTP status code.
+
     When the client creates a POST request to /users
-    And attaches a generic empty payload
+    And attaches a generic <payloadType> payload
     And sends the request
-    Then our API should respond with a 400 HTTP status code
+    Then our API should respond with a <statusCode> HTTP status code
     And the payload of the response should be a JSON object
-    And contains a message property which says "Payload should not be empty"
+    And contains a message property which says <message>
+
+    Examples:
+    | payloadType | statusCode | message |
+    | empty       | 400        | "Payload should not be empty" |
+    | non-JSON    | 415        | 'The "Content-Type" header must always be "application/json"' |
+    | malformed   | 400        | "Payload should be in JSON format"                           |
--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,25 +1,63 @@
+import assert from 'assert';
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
 
-When('the client creates a POST request to /users', function (callback) {
-    callback(null, 'pending')
-})
+When(/^the client creates a (GET|POST|PATCH|PUT|DELETE|OPTIONS|HEAD) request to ([/\w-:.]+)$/, function (method, path) {
+    this.request = superagent(
+        method,
+        `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}${path}`
+    );
+});
 
-When('attaches a generic empty payload', function (callback) {
-    callback(null, 'pending')
-})
+When(/^attaches a generic (.+) payload$/, function (payloadType) {
+    switch (payloadType) {
+        case 'malformed':
+            this.request
+                .send('{"email": "smartshader@gmail.com", name: }')
+                .set('Content-Type', 'application/json');
+            break;
+        case 'non-JSON':
+            this.request
+                .send('<?xml version="1.0" encoding="UTF-8" ?><email>smartshader@gmail.com</email>')
+                .set('Content-Type', 'text/xml');
+            break;
+        case 'empty':
+        default:
+    }
+});
 
-When('sends the request', function (callback) {
-    callback(null, 'pending')
-})
+When(/^sends the request$/, function (callback) {
+    this.request
+        .then((response) => {
+            this.response = response.res;
+            callback();
+        })
+        .catch((error) => {
+            this.response = error.response;
+            callback();
+        });
+});
 
-Then('our API should respond with a 400 HTTP status code', function (callback) {
-    callback(null, 'pending')
-})
+Then(/^our API should respond with a ([1-5]\d{2}) HTTP status code$/, function (statusCode) {
+    assert.strictEqual(this.response.statusCode, Number(statusCode));
+});
 
-Then('the payload of the response should be a JSON object', function (callback) {
-    callback(null, 'pending')
-})
+Then(/^the payload of the response should be a JSON object$/, function () {
+    // Check Content-Type header
+    const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+    if (!contentType || !contentType.includes('application/json')) {
+        throw new Error('Response not of Content-Type application/json');
+    }
 
-Then('contains a message property which says "Payload should not be empty"', function (callback) {
-    callback(null, 'pending')
-})
+    // Check it is valid JSON
+    try {
+        this.responsePayload = JSON.parse(this.response.text);
+    } catch (e) {
+        throw new Error('Response not a valid JSON object');
+    }
+});
+
+Then(/^contains a message property which says (?:"|')(.*)(?:"|')$/, function (message) {
+    assert.strictEqual(this.responsePayload.message, message);
+});
+
