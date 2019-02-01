6ab84c2b0d3171d8bbf4ba42c5aaf44f3d12d0d8
Imad Jerra <smartshader@gmail.com>
2019-02-01 16:34:26 -0500

    Extract request handlers into modules

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import checkEmptyPayload from './middlewares/check-empty-payload';
 import checkContentTypeIsSet from './middlewares/check-content-type-is-set';
 import checkContentTypeIsJson from './middlewares/check-content-type-is-json';
 import errorHandler from './middlewares/error-handler';
+import injectHandlerDependencies from './utils/inject-handler-dependencies';
+import createUser from './handlers/users/create';
 
 const client = new elasticsearch.Client({
   host: `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOST}:${process.env.ELASTICSEARCH_PORT}`,
@@ -25,53 +27,7 @@ app.use(checkContentTypeIsSet);
 app.use(checkContentTypeIsJson);
 app.use(errorHandler);
 
-app.post('/users', (req, res) => {
-  if (
-    !Object.prototype.hasOwnProperty.call(req.body, 'email')
-    || !Object.prototype.hasOwnProperty.call(req.body, 'password')
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'Payload must contain at least the email and password fields',
-    });
-  }
-
-  if (
-    typeof req.body.email !== 'string'
-    || typeof req.body.password !== 'string'
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'The email and password fields must be of type string',
-    });
-    return;
-  }
-
-  if (!/^[\w.+]+@\w+\.\w+$/.test(req.body.email)) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'The email field must be a valid email.',
-    });
-    return;
-  }
-
-  client.index({
-    index: process.env.ELASTICSEARCH_INDEX,
-    type: 'user',
-    body: req.body,
-  }).then((result) => {
-    res.status(201);
-    res.set('Content-Type', 'text/plain');
-    res.send(result._id);
-  }).catch(() => {
-    res.status(500);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Internal Server Error' });
-  });
-});
+app.post('/users', injectHandlerDependencies(createUser, client));
 
 app.listen(process.env.SERVER_PORT, () => {
   // eslint-disable-next-line no-console
